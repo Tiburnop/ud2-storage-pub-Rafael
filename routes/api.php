bf7a059efef36460ac6a5f2ba508795cab7bb782
Tiburnop <rafael.galvan.pomares@gmail.com>
2024-11-28 19:09:44 +0100

    25 test realizados

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HelloWorldController;
+use App\Http\Controllers\CsvController;
 
 
 Route::apiResource('hello', HelloWorldController::class);
@@ -16,12 +17,9 @@ Route::post('/json', [JsonController::class, 'store']);
 Route::get('/json/{filename}', [JsonController::class, 'show']);
 Route::put('/json/{filename}', [JsonController::class, 'update']);
 Route::delete('/json/{filename}', [JsonController::class, 'destroy']);
-
-use App\Http\Controllers\CsvController;
-
-// Rutas para CSV
+//Rutas para CSV
 Route::get('/csv', [CsvController::class, 'index']);
+Route::get('/csv/{id}', [CsvController::class, 'show']);
 Route::post('/csv', [CsvController::class, 'store']);
-Route::get('/csv/{filename}', [CsvController::class, 'show']);
-Route::put('/csv/{filename}', [CsvController::class, 'update']);
-Route::delete('/csv/{filename}', [CsvController::class, 'destroy']);
+Route::put('/csv/{id}', [CsvController::class, 'update']);
+Route::delete('/csv/{id}', [CsvController::class, 'destroy']);
